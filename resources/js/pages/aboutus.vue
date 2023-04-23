2b33c8f2f46d4544da263f9821100896d586e198
Sereyvathana <meng.sereyvathana@gmail.com>
2023-04-23 11:33:03 +0700

    about us

--- resources/js/pages/aboutus.vue
+++ resources/js/pages/aboutus.vue
@@ -11,23 +11,23 @@ const getDeveloper = async () => {
 </script>
 
 <template>
-    <h1 className='text-4xl mt-10 font-semibold text-primary text-center mb-10'>Our Developer</h1>
-    <div v-for="(item, index) in developers" className="mycontainer">
-        <div v-if="index % 2 == 0" className="flex flex-col md:flex-row gap-5 mt-5 justify-center mb-12">
-            <img :src="item.image" alt="" className='shadow-sm w-full md:w-[250px] h-[400px] md:h-[300px]  p-2 rounded-lg border border-solid border-gray-300 ' />
-            <div className='w-full md:w-[70%] order-first md:order-last'>
-                <h1 className='text-2xl font-semibold text-gray-800 mb-1'>{{ item.name }}</h1>
-                <h1 className='text-2xl font-semibold text-gray-600 mb-1'>{{ item.position }}</h1>
+    <h1 class='text-4xl mt-10 font-semibold text-primary text-center mb-10'>Our Developer</h1>
+    <div v-for="(item, index) in developers" class="mycontainer">
+        <div v-if="index % 2 == 0" class="flex flex-col md:flex-row gap-5 mt-5 justify-center mb-12">
+            <img :src="item.image" alt="" class='shadow-sm w-full md:w-[250px] h-[400px] md:h-[300px]  p-2 rounded-lg border border-solid border-gray-300 ' />
+            <div class='w-full md:w-[70%] order-first md:order-last'>
+                <h1 class='text-2xl font-semibold text-gray-800 mb-1'>{{ item.name }}</h1>
+                <h1 class='text-2xl font-semibold text-gray-600 mb-1'>{{ item.position }}</h1>
                 <p>{{ item.description }}</p>
             </div>
         </div>
 
-        <div v-else className="flex flex-col md:flex-row gap-5 mt-5 justify-center mb-12">
-            <img :src="item.image" alt="" className='shadow-sm w-full md:w-[250px] h-[400px] md:h-[300px]  p-2 rounded-lg border border-solid border-gray-300 ' />
-            <div className='w-full md:w-[70%] order-first'>
-                <h1 className='text-2xl font-semibold text-gray-800 mb-1'>{{ item.name }}</h1>
-                <h1 className='text-2xl font-semibold text-gray-600 mb-1'>{{ item.position }}</h1>
-                <p className='w-full overflow-hidden'>{{ item.description }}</p>
+        <div v-else class="flex flex-col md:flex-row gap-5 mt-5 justify-center mb-12">
+            <img :src="item.image" alt="" class='shadow-sm w-full md:w-[250px] h-[400px] md:h-[300px]  p-2 rounded-lg border border-solid border-gray-300 ' />
+            <div class='w-full md:w-[70%] order-first'>
+                <h1 class='text-2xl font-semibold text-gray-800 mb-1'>{{ item.name }}</h1>
+                <h1 class='text-2xl font-semibold text-gray-600 mb-1'>{{ item.position }}</h1>
+                <p class='w-full overflow-hidden'>{{ item.description }}</p>
             </div>
         </div>
     </div>

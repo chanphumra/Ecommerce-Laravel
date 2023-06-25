9c7fe2d4b8fed29fa699598514ccbdc4a154a839
chanphumra <chanphumracambodia@gmail.com>
2023-06-25 10:23:37 +0700

    test phpstom

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+//hello
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

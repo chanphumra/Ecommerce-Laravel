9e9deab847ba9a653588021fecd73a80e3510196
chanphumra <chanphumracambodia@gmail.com>
2023-06-25 10:26:21 +0700

    defualt

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-//hello
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

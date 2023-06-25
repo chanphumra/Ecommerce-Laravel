3139f41fe29a09039dd34968af0c0490b3bab3c2
chanphumra <chanphumracambodia@gmail.com>
2023-06-25 10:45:36 +0700

    Merge branch 'master' of https://github.com/chanphumra/Ecommerce-Laravel into vathana
     Conflicts:

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+//hahha
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
